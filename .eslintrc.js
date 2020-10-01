a3e8fc5464954c6e7c17357d6c7bed7e51c2383c
Jan Fischer <jan.fischer@nexum.de>
2020-10-01 20:47:47 +0200

    New yarn.lock, eslint issue, change cssprop import

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,9 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-use-before-define': [0],
+    // there seems to be an issue with @typescript-eslint
+    // without this an react import throws this error
+    '@typescript-eslint/no-loop-func': [0],
     'jest/expect-expect': [0],
     'import/prefer-default-export': [0],
     'react/jsx-props-no-spreading': [0],
