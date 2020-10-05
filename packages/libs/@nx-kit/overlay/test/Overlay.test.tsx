885c613edc3d6690a218d1ad38afe5cd6f442d51
Jan Fischer <jan.fischer@nexum.de>
2020-10-05 13:27:13 +0200

    test: fix tests

--- packages/libs/@nx-kit/overlay/test/Overlay.test.tsx
+++ packages/libs/@nx-kit/overlay/test/Overlay.test.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from '../../styling';
 import { theme } from '../../theme-default';
-import { Overlay } from '../src';
+import { Overlay, OverlayProvider } from '../src';
 
 describe('Overlay component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<ThemeProvider theme={theme}><Overlay /></ThemeProvider>, div);
+    ReactDOM.render(
+      <ThemeProvider theme={theme}>
+        <OverlayProvider>
+          <Overlay />
+        </OverlayProvider>
+      </ThemeProvider>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
