c8509ae56b2dbe3dadcb7b941ea751779f872ad5
Jan Fischer <jan.fischer@nexum.de>
2020-10-27 00:30:54 +0100

    feat(checkbox): try out styling checkbox (svg)

--- packages/libs/@nx-kit/checkbox/src/Checkbox.tsx
+++ packages/libs/@nx-kit/checkbox/src/Checkbox.tsx
@@ -4,6 +4,7 @@ import { useCheckbox } from '@react-aria/checkbox';
 import { useFocusRing } from '@react-aria/focus';
 import { mergeProps } from '@react-aria/utils';
 import { useToggleState } from '@react-stately/toggle';
+import { VisuallyHidden } from '@react-aria/visually-hidden';
 import {
   As,
   compose,
@@ -70,16 +71,42 @@ const Checkbox = (props: CheckboxProps, ref?: React.Ref<HTMLInputElement | null>
   };
 
   return (
-    <CheckboxStyled
-      ref={mergedRefs}
-      isFocused={isFocusVisible}
-      autoFocus={autoFocus}
-      isDisabled={isDisabled !== undefined}
-      hasError={hasError}
-      {...mergeProps(inputProps, focusProps, elementTypeProps, rest)}
-      aria-invalid={hasError ? true : undefined}
-      aria-required={isAriaRequired}
-    />
+    <div onClick={() => state.setSelected(!state.isSelected)}>
+      <VisuallyHidden>
+        <CheckboxStyled
+          ref={mergedRefs}
+          isFocused={isFocusVisible}
+          autoFocus={autoFocus}
+          isDisabled={isDisabled !== undefined}
+          hasError={hasError}
+          {...mergeProps(inputProps, focusProps, elementTypeProps, rest)}
+          aria-invalid={hasError ? true : undefined}
+          aria-required={isAriaRequired}
+        />
+      </VisuallyHidden>
+      <svg width={24} height={24} aria-hidden="true" style={{ marginRight: 4 }}>
+        <rect
+          x={state.isSelected ? 4 : 5}
+          y={state.isSelected ? 4 : 5}
+          width={state.isSelected ? 16 : 14}
+          height={state.isSelected ? 16 : 14}
+          fill={state.isSelected ? 'orange' : 'none'}
+          stroke={state.isSelected ? 'none' : 'gray'}
+          strokeWidth={2}
+        />
+        {state.isSelected && (
+          <path
+            transform="translate(7 7)"
+            d={`M3.788 9A.999.999 0 0 1 3 8.615l-2.288-3a1 1 0 1 1
+            1.576-1.23l1.5 1.991 3.924-4.991a1 1 0 1 1 1.576 1.23l-4.712
+            6A.999.999 0 0 1 3.788 9z`}
+          />
+        )}
+        {isFocusVisible && (
+          <rect x={1} y={1} width={22} height={22} fill="none" stroke="orange" strokeWidth={2} />
+        )}
+      </svg>
+    </div>
   );
 };
 
