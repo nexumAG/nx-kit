452d5832d141bf0ecdb4f0990a0f0bf642ad30a2
Jan Fischer <jan.fischer@nexum.de>
2021-05-27 21:55:14 +0200

    chore: update commands, storybook babel config no ssr

--- packages/apps/storybook/.storybook/babel.config.js
+++ packages/apps/storybook/.storybook/babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  // plugins: [['babel-plugin-styled-components', { ssr: false, displayName: true }]],
-  plugins: ['babel-plugin-styled-components'],
+  plugins: [['babel-plugin-styled-components', { ssr: false, displayName: true }]],
 };
