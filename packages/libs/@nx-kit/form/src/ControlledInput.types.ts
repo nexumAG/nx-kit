41c6292a6e4ffaf68cd4665e93d3e15ddbc50c55
Petr Glaser <12586960+pegak@users.noreply.github.com>
2022-10-04 09:54:20 +0000

    fix(form): add correct render props ref type

--- packages/libs/@nx-kit/form/src/ControlledInput.types.ts
+++ packages/libs/@nx-kit/form/src/ControlledInput.types.ts
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export type RenderProps = {
+export type RenderProps<T extends HTMLElement = HTMLElement> = {
   onBlur: () => void;
   onChange: (value: any) => void;
   value: React.Key;
   hasError: boolean;
-  ref: React.Ref<HTMLElement>;
+  ref: React.Ref<T>;
 };
 
 export type ControlledInputProps = {
